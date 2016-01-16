b8c808b67f6ce5c8bf7c23429293664c28e28c7a
Bernardo Lins <bernardolins28@gmail.com>
2016-01-16 16:44:44 -0200

    Create metadata builder

---
+++ metadata/builder.go
@@ -0,0 +1,27 @@
+package metadata
+
+import (
+	"github.com/bernardolins/vandame/file"
+	"gopkg.in/yaml.v2"
+	"log"
+	"os"
+)
+
+func Build(bytes []byte) Specification {
+	spec := serialize(bytes)
+
+	return spec
+}
+
+func serialize(metadata []byte) Specification {
+	spec := Specification{}
+
+	err := yaml.Unmarshal(metadata, &spec)
+
+	if err != nil {
+		log.Fatalf("-- error %v", err)
+		os.Exit(1)
+	}
+
+	return spec
+}
