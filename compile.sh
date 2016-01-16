53b23ed680e303c68acae0523dc300ff54dec535
Bernardo Lins <bernardolins28@gmail.com>
2016-01-16 16:39:04 -0200

    Fix compile script include

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 GOVERSION="1.5"
-docker run --rm -v "$PWD":/usr/src/vandame -w /usr/src/vandame golang:${GOVERSION} go build -v
+docker run --rm -v "$PWD":/go/src/github.com/bernardolins/vandame -w /go/src/github.com/bernardolins/vandame golang:${GOVERSION} go build -v
