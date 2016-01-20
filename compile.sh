80668335276ce6bff7a72224bfa71d95fb0a9381
Bernardo Lins <bernardolins28@gmail.com>
2016-01-20 14:46:49 -0200

    Add go get command to compile script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 GOVERSION="1.5"
-docker run --rm -v "$PWD":/go/src/github.com/bernardolins/vandame -w /go/src/github.com/bernardolins/vandame golang:${GOVERSION} go build -v
+docker run --rm -v "$PWD":/go/src/github.com/bernardolins/vandame -w /go/src/github.com/bernardolins/vandame golang:${GOVERSION} go get ./... && go build -v 
